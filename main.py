6269abdd6af3541a7173d3bd7ecb173f278bcfeb
gabicantarini <gabilana64@hotmail.com>
2022-01-26 21:54:57 +0000

    feat: adiciona app para validar cpf

---
+++ main.py
@@ -0,0 +1,27 @@
+from Cpf import Cpf
+
+cpf = "15616987913"
+
+
+
+
+fatia_um = cpf[:3]
+fatia_dois = cpf[3:6]
+fatia_tres = cpf[6:9]
+fatia_quatro = cpf[9:]
+print(fatia_um)
+print(fatia_dois)
+print(fatia_tres)
+print(fatia_quatro)
+
+
+
+
+
+
+#objeto_cpf = Cpf(cpf)
+
+#cpf = 15616987913
+#cpf = str(cpf)
+#tamanho_cpf = len(cpf)
+#print(tamanho_cpf)
